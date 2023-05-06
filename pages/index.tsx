6e49f17b166781031347a31a89f29fc0fe692dfc
Y0ngTae-Kim <ista1024@gmail.com>
2023-05-06 18:33:43 +1000

    separate Layout from index add mongoose connect function

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,41 +1,21 @@
-import { Inter } from "next/font/google";
-import { useSession, signIn, signOut } from "next-auth/react";
-
-const inter = Inter({ subsets: ["latin"] });
+import Layout from "@/components/Layout";
+import { useSession } from "next-auth/react";
 
 export default function Home() {
   const { data: session } = useSession();
-  if (!session)
-    return (
-      <div className="bg-blue-900 v-screen h-screen flex items-center">
-        <div className="text-center w-full">
-          <button
-            onClick={() => signIn("google")}
-            className="bg-white p-2 px-4 rounded-lg"
-          >
-            Login with Google
-          </button>
-        </div>
-      </div>
-    );
   return (
-    <div className="bg-blue-900 v-screen h-screen flex items-center">
-      <div className="w-full">
-        <div className="bg-white">
-          <p>
-            logged in
-            <pre>{JSON.stringify(session.user, null, 2)}</pre>
-          </p>
-        </div>
-        <div className="text-center ">
-          <button
-            onClick={() => signOut()}
-            className="bg-white p-2 px-4 rounded-lg"
-          >
-            Logout
-          </button>
+    <Layout>
+      <div className="text-blue-900 flex justify-between">
+        <h2>
+          Hello, <b>{session?.user?.name}</b>
+        </h2>
+        <div className="flex bg-gray-300 text-black rounded-lg overflow-hidden">
+          <img src={session?.user?.image || ""} alt="" className="w-6 h-6" />
+          <span className="px-3">
+            {session?.user?.name}
+          </span>
         </div>
       </div>
-    </div>
-  );
+    </Layout>
+  )
 }
